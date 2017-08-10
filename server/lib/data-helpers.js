b9416b4139a0ac26542d478a4582645435973f62
hounslow <hounslowmatthew@gmail.com>
2017-08-10 20:46:43 +0000

    saveTweet and getTweets now work

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,12 +6,11 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
+        db.collection('tweets').insertOne(newTweet, callback(null, true));
+        //callback(null, true);
       });
     },
 
@@ -19,7 +18,7 @@ module.exports = function makeDataHelpers(db) {
     getTweets: function(callback) {
       simulateDelay(() => {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        db.collection('tweets').find({}).toArray(callback);
       });
     }
 
